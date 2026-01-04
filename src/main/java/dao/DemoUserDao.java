3b8ab908da8748e03e19e5dbbee520f76bb4f751
seli <meziniseli@gmail.com>
2026-01-04 22:18:07 +0100

    fixing smell issues

--- src/main/java/dao/DemoUserDao.java
+++ src/main/java/dao/DemoUserDao.java
@@ -31,7 +31,7 @@ public class DemoUserDao extends UserDao {
         //  utenti di esempio
         users.add(new Citizen("Seli", "CivisAlert", "Citizen"));
         users.add(new Employee("SeliEmployee", "password", "Employee"));
-        Employee employee = new Employee("balotelli", "password", "Employee");
+       // Employee employee = new Employee("balotelli", "password", "Employee");
 
     }
 
